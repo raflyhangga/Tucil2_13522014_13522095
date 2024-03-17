778c70342a27d0ee01e55a15c708cd4afc6c1558
Rayhan Fadhlan <65324331+RayhanFadhlan@users.noreply.github.com>
2024-03-18 00:06:39 +0700

    add n control brute

--- api/BezierCurve.py
+++ api/BezierCurve.py
@@ -1,59 +1,14 @@
-import matplotlib.pyplot as plt
 from typing import List
 from dataclasses import dataclass
 import math
 import time
+
+
 class Point:
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
-class Bezier:
-    def __init__(self,iterasi = 3, ctr_pts = list()) -> None:
-        self.bezier_points:list[tuple[float,float]] = []
-        self.ctrl_points:list[tuple[float,float]] = ctr_pts
-        self.max_iteration = iterasi
-
-    # Get the middle point between (x1,y1) and (x2,y2)
-    def midPoints(self,ctrl1: tuple[float,float],ctrl2: tuple[float,float]):
-        return ((ctrl1[0] + ctrl2[0]) / 2,(ctrl1[1] + ctrl2[1]) / 2)
-
-    # Populating the points recursively with amount of n iterations
-    def populate(
-                    self,
-                    ctrl1: tuple[float,float],
-                    ctrl2: tuple[float,float],
-                    ctrl3:tuple[float,float],
-                    iterasi: int,
-                    result:list[tuple[float,float]]
-                ) -> None:
-        if (iterasi <= self.max_iteration):
-            midpoint1 = self.midPoints(ctrl1,ctrl2)
-            midpoint2 = self.midPoints(ctrl2,ctrl3)
-            midpoint3 = self.midPoints(midpoint1,midpoint2)
-            iterasi = iterasi + 1
-            self.populate(ctrl1,midpoint1,midpoint3,iterasi,result)
-            result.append(midpoint3)
-            self.populate(midpoint3,midpoint2,ctrl3,iterasi,result)
-
-    # Create Canvas
-    def createBezier(self) -> None:
-        result:list[tuple[float,float]] = []
-        c1,c2,c3 = self.ctrl_points
-        self.populate(c1,c2,c3,0,result)
-        self.bezier_points = result
-    
-    # Plotting the points
-    def draw(self) -> None:
-        x_coords, y_coords = zip(*self.bezier_points)
-        plt.plot(x_coords, y_coords, label="Bézier Curve")
-        plt.scatter(*zip(*self.ctrl_points), color="red", label="Control Points")
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.title("Bézier Curve")
-        plt.legend()
-        plt.grid(True)
-        plt.show()
 
 class BezierCurve:
     def __init__(self, control_points: List[Point], iteration : int): 
@@ -76,7 +31,6 @@ class BezierCurve:
         count_ctrl = len(self.control_points)
         for i in range(jml_titik - 2):
             t = (i+1) * beda
-
             x = 0
             y = 0
             for j in range(count_ctrl):
@@ -94,7 +48,6 @@ class BezierCurve:
         execution_time = (end_time - start_time) * 1000
         self.brutal_execution_time = int(execution_time)
 
-
     def create_bezier_dnc(self):
 
         start_time = time.time()
@@ -106,6 +59,8 @@ class BezierCurve:
         end_time = time.time()
         execution_time = (end_time - start_time) * 1000
         self.dnc_execution_time = int(execution_time)
+        
+
 
     def calculate_new_point(self,ctrl_points : List[Point],current_iteration : int):
         if current_iteration < self.iteration:
@@ -145,13 +100,14 @@ class BezierCurve:
         self.print_points(self.result_points_brutal)
 
     def print_iter_res(self):
-        for points in self.iter_res:
-            self.print_points(points)
+        for i in range(len(self.iter_res)):
+            print("Iterasi ke-",i+1)
+            self.print_points(self.iter_res[i])
 
-    def get_result_points_dnc(self) -> list[Point]:
+    def get_result_points_dnc(self):
         return self.result_points_dnc
     
-    def get_result_points_brutal(self) -> list[Point]:
+    def get_result_points_brutal(self):
         return self.result_points_brutal
     
     def get_control_points(self):
@@ -177,3 +133,5 @@ class BezierCurve:
             for j in range(0,len(self.result_points_dnc),math.floor(step)):
                 temp.append(self.result_points_dnc[j])
             self.iter_res.append(temp)
+        
+    
